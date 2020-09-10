fcd1c635f432f97edc6e14bae85d81a01c500ee0
Birojit Mondal <BIROJIT.MONDAL@lntinfotech.com>
2020-09-10 15:20:11 +0530

    forjenkins1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        int iNum1 = 0, iNum2 = 0;
+
+        void GetNumbers()
+        {
+            Console.WriteLine("enter 1 no=");
+            while (!Int32.TryParse(Console.ReadLine(), out iNum1)) { Console.WriteLine("Enter no 1 again="); }
+            Console.WriteLine("enter 2 no=");
+            while (!Int32.TryParse(Console.ReadLine(), out iNum2)) { Console.WriteLine("Enter no 2 again="); }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.GetNumbers();
+            Console.WriteLine("{0}\n{1}\n", p.iNum1, p.iNum2);
+        }
+    }
+}
